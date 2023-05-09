0add6c4cbca8a3c36e7740e3c7f84bd4c95c4aaf
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2023-05-09 23:56:34 +0200

    tests committed

---
+++ test/unit_test.dart
@@ -0,0 +1,33 @@
+import 'package:evolt_test/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:evolt_test/presentation/register_user.dart';
+
+void main() {
+  group('RegisterUser widget', () {
+    testWidgets('has an elevated button', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: RegisterUser()));
+
+      final elevatedButtonFinder = find.byType(ElevatedButton);
+
+      expect(elevatedButtonFinder, findsOneWidget);
+    });
+
+    testWidgets('displays welcome text', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: RegisterUser()));
+
+      final welcomeTextFinder = find.text('Welcome to We Text');
+
+      expect(welcomeTextFinder, findsOneWidget);
+    });
+
+    testWidgets('displays connect text', (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+
+      final connectTextFinder =
+          find.textContaining('Connect with your friends here!');
+
+      expect(connectTextFinder, findsOneWidget);
+    });
+  });
+}
